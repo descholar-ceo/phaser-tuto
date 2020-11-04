f4cd325f8aa757b2bf0949347cba3eb4384a2dcf
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-04 12:05:49 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = (env) => ({
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  mode: env ? 'production' : 'development',
+  devServer: {
+    inline: true,
+    port: process.env.PORT,
+    contentBase: path.join(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: 'Phaser Tuto',
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: 'index.html',
+    }),
+  ],
+  performance: {
+    hints: false,
+  },
+});
