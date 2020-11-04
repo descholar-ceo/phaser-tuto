6c47087bd4103766d9f231eb88f95b5ae625dab2
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-04 13:37:04 +0200

    load assets

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ const config = {
   type: Phaser.AUTO,
   width: 1000,
   height: 400,
-  physics: {
-      default: 'arcade',
-      arcade: {
-          gravity: { y: 0 }
-      }
-  },
   scene: {
       preload: preload,
       create: create,
@@ -20,33 +14,17 @@ const config = {
 new Phaser.Game(config);
 
 function preload() {
-  this.load.setBaseURL('http://labs.phaser.io');
-
-  this.load.image('sky', 'assets/skies/space3.png');
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-  this.load.image('red', 'assets/particles/red.png');
-};
+  this.load.image('sky','assets/sky.png')
+  this.load.image('ground','assets/platform.png')
+  this.load.image('star','assets/star.png')
+  this.load.image('bomb','assets/bomb.png')
+  this.load.spritesheet('dude','assets/dude.png',{frameWidth: 32, frameHeight: 48})
+}
 
 function create (){
-  this.add.image(400, 300, 'sky');
-
-  var particles = this.add.particles('red');
-
-  var emitter = particles.createEmitter({
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: 'ADD'
-  });
-
-  var logo = this.physics.add.image(400, 100, 'logo');
-
-  logo.setVelocity(100, 200);
-  logo.setBounce(1, 1);
-  logo.setCollideWorldBounds(true);
-
-  emitter.startFollow(logo);
+  
 }
 
 function update() {
-  
+
 }
