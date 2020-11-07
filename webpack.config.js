a425a9330aef5d3941344520a5aad0dbb098c78a
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-07 15:25:32 +0200

    Change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,19 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const webpack = require('webpack');
 
-module.exports = (env) => ({
-  context: __dirname,
-  entry: { app: './src/index.js' },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.main.js',
+module.exports = {
+  entry: {
+    app: './src/index.js',
   },
-  mode: env ? 'production' : 'development',
-  devServer: {
-    inline: true,
-    port: process.env.PORT,
-    contentBase: path.join(__dirname, 'dist'),
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js',
   },
 
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
         include: path.resolve(__dirname, 'src/'),
         use: {
           loader: 'babel-loader',
@@ -31,18 +24,4 @@ module.exports = (env) => ({
       },
     ],
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      title: 'Phaser Tuto',
-      template: path.resolve(__dirname, 'public/index.html'),
-      filename: 'index.html',
-    }),
-    new webpack.DefinePlugin({
-      'typeof CANVAS_RENDERER': JSON.stringify(true),
-      'typeof WEBGL_RENDERER': JSON.stringify(true),
-    }),
-  ],
-  performance: {
-    hints: false,
-  },
-});
+};
