f009f90582d29f49e290edc43185dcd663afeaa9
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-07 17:20:55 +0200

    Remove messagers

--- src/messager.js
+++
@@ -1,7 +0,0 @@
-function showMessage(messageText) {
-  const messageEl = document.createElement('h1');
-  messageEl.textContent = messageText;
-  document.body.appendChild(messageEl);
-}
-
-export default showMessage;
