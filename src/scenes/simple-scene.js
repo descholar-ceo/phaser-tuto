a38d09da4d90d39b45a3e2e1a29c9a5e9dd3b22d
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-08 21:22:35 +0200

    Add assets

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -3,10 +3,17 @@ import Phaser from 'phaser';
 class SimpleScene extends Phaser.Scene {
   preload() {
     this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    // this.load.spritesheet('dude', 'assets/dude.png');
   }
 
   create() {
-    this.add.image(100, 200, 'sky');
+    this.add.image(400, 300, 'sky');
+    this.add.image(100, 200, 'ground');
+    this.add.image(100, 200, 'star');
+    this.add.image(200, 200, 'bomb');
   }
 }
 
