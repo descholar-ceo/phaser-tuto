12637fd6338c2ef21bbcbd61c0d81f53f3f988f9
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-08 21:36:38 +0200

    Use index.js file

--- src/index.js
+++ src/index.js
@@ -1,35 +1,27 @@
-// import Phaser from 'phaser';
-// // import sky from '../assets/sky.png';
-// // import ground from '../assets/platform.png';
-// // import star from '../assets/star.png';
-// // import bomb from '../assets/bomb.png';
-// // import dude from '../assets/dude.png';
-
 import Phaser from 'phaser';
-import SimpleScene from './scenes/simple-scene';
 
-// function preload() {
-//   this.load.setBaseURL(__dirname);
-//   this.load.image('sky', '../assets/sky.png');
-//   this.load.image('ground', '../assets/platform.png');
-//   this.load.image('star', '../assets/star.png');
-//   this.load.image('bomb', '../assets/bomb.png');
-//   this.load.spritesheet('dude', '../assets/dude.png', { frameWidth: 32, frameHeight: 48 });
-// }
+function preload() {
+  this.load.setBaseURL(__dirname);
+  this.load.image('sky', '../assets/sky.png');
+  this.load.image('ground', '../assets/platform.png');
+  this.load.image('star', '../assets/star.png');
+  this.load.image('bomb', '../assets/bomb.png');
+  this.load.spritesheet('dude', '../assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+}
 
-// function create() {
-//   this.add.image(400, 300, 'sky');
-//   this.add.image(400, 300, 'star');
-// }
+function create() {
+  this.add.image(400, 300, 'sky');
+  this.add.image(400, 300, 'star');
+}
 
-// function update() {
+function update() {
 
-// }
+}
 
 const config = {
   width: 800,
   height: 600,
-  scene: SimpleScene,
+  scene: { preload, create, update },
 };
 
 // eslint-disable-next-line no-unused-vars
