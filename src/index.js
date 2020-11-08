abd821551ee3cd4e37fd91e8e8d32ffb895d4daf
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-08 21:48:54 +0200

    Add more grounds

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ function create() {
   this.add.image(400, 300, 'sky');
   platforms = this.physics.add.staticGroup();
   platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+  platforms.create(600, 400, 'ground');
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
 }
